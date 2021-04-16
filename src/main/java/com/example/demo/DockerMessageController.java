243c0ae5cc9132ba7c6d19aa5a7949f03128221b
sriramede9 <48334851+sriramede9@users.noreply.github.com>
2021-04-16 11:12:30 -0400

    demo-docker file

---
+++ src/main/java/com/example/demo/DockerMessageController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerMessageController {
+	
+	@Value("${msg}")
+	private String msg;
+
+	@GetMapping("/messages")
+    public String getMessage() {
+        return "Hello from Docker!"+" \t"+msg;
+    }
+}
